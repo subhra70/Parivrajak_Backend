d796173245402c5ed39de28a7935196c14bf9886
Subhra704 <shawsubhra68@gmail.com>
2025-06-28 23:53:50 +0530

    frontend link updated 2

--- src/main/java/com/subhrashaw/ParivrajakBackend/config/SecurityConfig.java
+++ src/main/java/com/subhrashaw/ParivrajakBackend/config/SecurityConfig.java
@@ -156,7 +156,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://parivrajak-q62qdnuk5-subhra-shaws-projects.vercel.app/"));
+        config.setAllowedOrigins(List.of("https://parivrajak-q62qdnuk5-subhra-shaws-projects.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
